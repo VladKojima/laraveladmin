5bc7a0d6eac95f291f8a1f46efb53dae5f6f8a39
VladKojima <VladKojima@users.noreply.github.com>
2024-12-10 14:02:00 +0400

    objTable improve

--- src/components/objTable.js
+++ src/components/objTable.js
@@ -1,16 +1,21 @@
 import { useState } from 'react';
 import style from './style.module.css';
+import { idSymbol } from '../utils/idSymbol';
 
 export function ObjectTable({
     objects,
     columns,
     editableColumns,
+    schema,
     onSave,
 }) {
     const [objectsState, setObjectsState] = useState(objects);
 
+    const keys = Object.keys(objects[0]);
+
     function reset() {
         setObjectsState(objects);
+        setEditableObject(null);
     }
 
     function setProp(object, prop, value) {
@@ -21,49 +26,56 @@ export function ObjectTable({
         }))
     }
 
+    function add() {
+        setObjectsState([
+            ...objectsState,
+            { ...Object.fromEntries(keys.map(k => [k, undefined])), [idSymbol]: crypto.randomUUID() }
+        ])
+    }
+
+    function remove(object) {
+        setObjectsState(objectsState.filter(obj => obj !== object));
+    }
+
     const [editableObject, setEditableObject] = useState(null);
-    const [editableProperty, setEditableProperty] = useState(null);
 
     return <div>
         <table className={style.table}>
             <thead className={style.thead}>
-                <tr>{(Object.keys(objects[0]))
+                <tr>{keys
                     .filter(k => !columns || columns.includes(k))
                     .map(
                         k => <td key={k} className={style.td}>{k}</td>
                     )}
+                    <td className={style.td}>Действия</td>
                 </tr>
             </thead>
             <tbody className={style.tbody}>
-                {objectsState.map(obj => <tr key={obj.id} className={style.tr}>
+                {objectsState.map(obj => <tr key={obj.id ?? obj[idSymbol]} className={[style.tr, editableObject === (obj.id ?? obj[idSymbol]) && style.editable].join(" ")}>
                     {Object.entries(obj)
                         .filter(([k, v]) => !columns || columns.includes(k))
                         .map(([k, v]) => <td
                             key={k}
                             className={style.td}
-                            onDoubleClick={() => {
-                                if (k !== 'id' && (editableColumns === true ||
-                                    !!editableColumns && editableColumns?.includes(k))) {
-                                    setEditableObject(obj.id);
-                                    setEditableProperty(k);
-                                }
-                            }}
                         >
                             <input
                                 className={style.field}
-                                onBlur={() => {
-                                    setEditableObject(null);
-                                    setEditableProperty(null);
-                                }}
-                                value={v} onChange={({ target: { value } }) => setProp(obj, k, value)}
-                                readOnly={!(editableObject === obj.id && editableProperty === k)}
+                                value={!!v ? v : ""}
+                                onChange={({ target: { value } }) => setProp(obj, k, value)}
+                                disabled={k === 'id' || editableObject !== (obj.id ?? obj[idSymbol])}
+                                type={schema?.[k] ?? 'text'}
                             />
                         </td>)}
+                    <td className={style.td}>
+                        <button onClick={() => setEditableObject(obj.id ?? obj[idSymbol])}>Редактировать</button>
+                        <button onClick={() => remove(obj)}>Удалить</button>
+                    </td>
                 </tr>)}
             </tbody>
         </table>
         {
-            editableColumns && <div>
+            editableColumns && <div className={style.buttons}>
+                <button onClick={add}>Добавить</button>
                 <button onClick={reset}>Сбросить</button>
                 <button onClick={() => { onSave(objectsState); }}>Сохранить</button>
             </div>
---
+++ src/utils/idSymbol.js
@@ -0,0 +1,1 @@
+export const idSymbol = Symbol();
