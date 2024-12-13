0aa1c41de6085034dc292176e659a527933cd299
Your Name <you@example.com>
2024-12-14 02:57:21 +0400

    fix login error show

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -13,12 +13,16 @@ export function LoginPage({ onLogin }) {
             .then(res => {
                 sessionStorage.setItem("token", res.data.token);
                 onLogin();
+            })
+            .catch(err => {
+                console.log(err);
+                alert('Ошибка авторизации.');
             });
     }
 
     return <form className={style.form} onSubmit={submit}>
         <input value={email} onChange={({ target: { value } }) => setEmail(value)} placeholder="email" />
-        <input value={password} onChange={({ target: { value } }) => setPassword(value)} placeholder="password" />
+        <input type="password" value={password} onChange={({ target: { value } }) => setPassword(value)} placeholder="password" />
 
         <button type="submit">Войти</button>
     </form>
